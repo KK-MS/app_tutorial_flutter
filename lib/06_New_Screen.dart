30d101c7aa67fc68c058bc20b5c2ebaa76b6d225
shraddhajogani <53086093+shraddhajogani@users.noreply.github.com>
2019-08-20 13:04:55 +0200

    Add files via upload

---
+++ lib/06_New_Screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SecondScreen());
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+       appBar: AppBar(backgroundColor: Colors.grey,
+      title: Text('Welcome to Second Screen'),
+       ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Please prssed this button',),
+          ),
+        ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+      child: Icon(Icons.add,
+      ),
+        onPressed: (){
+
+        }
+      ),
+      );
+  }
+}
